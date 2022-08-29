fdda97998c54a76fc2423120ed190d419b9e1dc7
THEODOREALO <tjaesa@gmail.com>
2022-08-29 13:42:00 -0700

    navbar added but still need to style home

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,13 @@
+import Navbar from "./components/navbar";
 function App() {
   return (
-    <div></div>
+    <div>
+      <Navbar></Navbar>
+      <div id="body">
+        <b>You got power in your corner now</b>
+        <p>Use AI and Machine Learning to predict fights</p>
+      </div>
+    </div>
   );
 }
 
