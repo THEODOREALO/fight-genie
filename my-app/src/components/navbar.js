03618b5229c3645a4202dd140ef60cb0b526e418
THEODOREALO <tjaesa@gmail.com>
2022-08-29 11:30:00 -0700

    Added navbar

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../css/navbar.css';
 //Replace "Template" with the name of your component
 class Navbar extends React.Component{
     //Place any instance variables here; if no instance variables, constructor can be removed
@@ -11,7 +11,23 @@ class Navbar extends React.Component{
     //Place all html to return here
     render(){
         return(
-            <div>Hello</div>
+            <div id='nav'>
+                <section id='text'>
+                    <section id='title'>
+                        Fight Genie
+                    </section>
+                    <section id='account'>
+                        <div id='account-buttons'>
+                            <div id='login'>
+                            Login
+                            </div>
+                            <button id='signup'>
+                            Signup
+                            </button>
+                        </div>
+                    </section>
+                </section>
+            </div>
         );
     };
 }
